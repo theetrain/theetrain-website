5ca5db401f56bc2606920df4d61d47800f3fd3cd
Enrico Sacchetti <enrico@theetrain.ca>
2026-05-30 16:34:58 -0400

    fix: split sitemap.xml to its own route
    Routes with content negotiation cannot be prerendered. This may
    potentially be fixed by https://github.com/sveltejs/kit/pull/15621

--- src/routes/sitemap/+page.server.ts
+++ src/routes/sitemap/+page.server.ts
@@ -2,8 +2,6 @@ import { allPosts } from 'content-collections'
 import { format } from 'date-fns'
 import type { PageServerLoad } from './$types'
 
-export const prerender = true
-
 export const load: PageServerLoad = async () => {
   const posts = allPosts.map((post) => ({
     slug: post.slug,
